0b462e20c6dd40ee886c545e796e1bc484cea312
aaronmt1011 <aaronmt1011@gmail.com>
2023-02-09 15:51:52 -0700

    Uploaded semi-finished board made using CSS Grid

--- javascript.js
+++ javascript.js
@@ -1,13 +1,29 @@
 const gameBoard = document.querySelector('.game-board');
 
-function board() {
-    for(let i = 0; i < 256; i++) {
+
+function board16() {
+    for(let i = 1; i < (16*16); i++) {
         let gameBox = document.createElement('div');
         gameBox.classList.add('game-box');
+        gameBoard.classList.add('g17');
         gameBoard.appendChild(gameBox);
     }
 }
 
-board();
+function board17() {
+    for(let i = 0; i < (17*17); i++) {
+        let gameBox = document.createElement('div');
+        gameBox.classList.add('game-box');
+        gameBoard.classList.remove('g16');
+        gameBoard.classList.add('g17');
+        gameBoard.appendChild(gameBox);
+    }
+}
 
-console.log(gameBoard);
+
+board16();
+
+
+
+
+
