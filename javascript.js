873ceed757d9ac056772c682b18479e2c5d177af
aaronmt1011 <aaronmt1011@gmail.com>
2023-03-03 10:37:03 -0700

    Fixed buttons options for gameboard size

--- javascript.js
+++ javascript.js
@@ -1,29 +1,63 @@
 const gameBoard = document.querySelector('.game-board');
+const game16 = document.querySelector('.game-16');
+const game17 = document.querySelector('.game-17');
+let gameBoardSize = document.getElementById("board-size");
 
+function boardSize() {
+    boardSize = document.getElementById("board-size").value;
+    console.log(boardSize);
+}
+
+function boardReset() {
+    gameBoard.classList.add('g-reset');
+    gameBoard.classList.remove('g-16');
+    gameBoard.classList.remove('g-17');
+
+    /* 
+    While loop is used to loop through each elemet of gameBoard (gameBox) and 
+    removes them so the board is cleard.
+    */
+    while (gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild)
+    }
+}
 
 function board16() {
-    for(let i = 1; i < (16*16); i++) {
+    boardReset();
+    for(let i = 1; i <= (16 * 16); i++) {
         let gameBox = document.createElement('div');
         gameBox.classList.add('game-box');
-        gameBoard.classList.add('g17');
         gameBoard.appendChild(gameBox);
     }
+    gameBoard.classList.add('g-16');
 }
 
 function board17() {
-    for(let i = 0; i < (17*17); i++) {
+    boardReset();
+    for(let i = 1; i <= (17 * 17); i++) {
         let gameBox = document.createElement('div');
         gameBox.classList.add('game-box');
-        gameBoard.classList.remove('g16');
-        gameBoard.classList.add('g17');
         gameBoard.appendChild(gameBox);
     }
+    gameBoard.classList.add('g-17');
 }
 
+game16.addEventListener('click', board16);
+game17.addEventListener('click', board17);
+gameBoardSize.addEventListener('click', boardSize);
+gameBoardSize.addEventListener('mouseover', boardSize);
 
-board16();
 
 
+/*
+game16.addEventListener('click', () => {
+    boardReset();
+    board16();    
+});
 
-
+game17.addEventListener('click', () => {
+    boardReset();
+    board17();    
+});
+*/
 
