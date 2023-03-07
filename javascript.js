608ebf94521b2d758860c912310aa681b2ee38dc
aaronmt1011 <aaronmt1011@gmail.com>
2023-03-07 13:11:01 -0700

    Fixed game board size issue

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 const gameBoard = document.querySelector('.game-board');
 let gameBoardSize = document.getElementById("board-size");
 let sizeDisplay = document.querySelector('.range-number');
-
-
+let gameBox = document.createElement('div');
+gameBox.classList.add('game-box');
 
 /*
 boardSize() takes the value from the slider and uses it to change the board size
@@ -13,11 +13,12 @@ function boardSize() {
     sizeDisplay.textContent = `${a} x ${a}`;
     boardReset();
     gameBoard.classList.remove('g-reset');
-    gameBoard.style.gridTemplateColumns = `repeat(${a}, 22px)`
-    gameBoard.style.gridTemplateRows = `repeat(${a}, 22px)`
+    gameBoard.style.gridTemplateColumns = `repeat(${a}, auto)`
+    gameBoard.style.gridTemplateRows = `repeat(${a}, auto)`
     for(let i = 1; i <= (a * a); i++) {
-        let gameBox = document.createElement('div');
+        gameBox = document.createElement('div');
         gameBox.classList.add('game-box');
+        gameBox.classList.add(`gbox${i}`);
         gameBoard.appendChild(gameBox);
     }
 }
@@ -45,6 +46,24 @@ function boardReset() {
     }
 }
 
+
+function colorChange() {
+    gameBox.classList.add('g-b-color');
+    console.log('hey');
+}
+
+[...document.querySelectorAll(".game-box")].forEach((round) => {
+    round.addEventListener("click", function() {
+      round.style.backgroundColor = "blue";
+    })
+  })
+
+
+gameBoardSize.addEventListener('click', boardSize);
+gameBoardSize.addEventListener('mouseover', boardSize);
+gameBox.addEventListener('mouseover', colorChange);
+gameBox.addEventListener('onmousemove', colorChange);
+boardSize();
 
 
 /*
@@ -78,10 +97,6 @@ function board17() {
 game16.addEventListener('click', board16);
 game17.addEventListener('click', board17);
 */
-gameBoardSize.addEventListener('click', boardSize);
-gameBoardSize.addEventListener('mouseover', boardSize);
-boardSize();
-
 
 /*
 game16.addEventListener('click', () => {
